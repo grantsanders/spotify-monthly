c70d1ba8dbb42ea78d71fd171811c0f667922c62
grantsanders <gsanders@ecs-online.com>
2024-01-20 17:53:19 -0500

    cleaning up main()

--- src/main.go
+++ src/main.go
@@ -20,37 +20,31 @@ var (
 )
 
 func init() {
-	os.Setenv("SPOTIFY_ID", "ID")
-	os.Setenv("SPOTIFY_SECRET", "Secret")
+
+	// these are for testing only
+	os.Setenv("SPOTIFY_ID", "d892b49996e1409cb64e9665ae2108e9")
+	os.Setenv("SPOTIFY_SECRET", "4255b0a88102430fb49349a5efa38957")
+
 	const redirectURI = "http://localhost:1000/callback"
 
 	auth = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate, spotifyauth.ScopeUserTopRead, spotifyauth.ScopeUserLibraryModify))
 	ch = make(chan *spotify.Client)
-	state = "abc123"
+	state = "the1"
 }
 
 func main() {
-	http.HandleFunc("/callback", completeAuth)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got request for:", r.URL.String())
-	})
 
-	go func() {
-		err := http.ListenAndServe(":1000", nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	url := auth.AuthURL(state)
-	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+	configureServer()
 
 	client := <-ch
+
+	schedulePlaylistCreation()
 
 	user, err := client.CurrentUser(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println("You are logged in as:", user.ID)
 
 	tracks, err := client.CurrentUsersTopTracks(context.Background(), spotify.Timerange(spotify.ShortTermRange), spotify.Limit(30))
@@ -58,7 +52,7 @@ func main() {
 		log.Fatalf("Error fetching top tracks: %v", err)
 	}
 	for _, track := range tracks.Tracks {
-		fmt.Println(track.Name)
+		fmt.Println(track.ID)
 	}
 
 }
@@ -73,9 +67,32 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		log.Fatalf("State mismatch: %s != %s\n", st, state)
 	}
-
 	// use the token to get an authenticated client
 	client := spotify.New(auth.Client(r.Context(), tok))
 	fmt.Fprintf(w, "Login Completed!")
 	ch <- client
+}
+
+func schedulePlaylistCreation() {
+
+}
+
+func configureServer() {
+
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+
+	go func() {
+		err := http.ListenAndServe(":1000", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
 }
