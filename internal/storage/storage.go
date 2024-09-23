fe8ca8552230c7895da60eddcd49b2e3f05cf630
grantsanders <gsanders@ecs-online.com>
2024-09-23 18:00:29 -0400

    removed built in scheduler, added token storage to cloud DB to avoid manual login requirement

---
+++ internal/storage/storage.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"golang.org/x/oauth2"
+)
+
+func openDBConnection() (*sql.DB, error) {
+	connStr := "***REMOVED***"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func StoreNewToken(token *oauth2.Token) error {
+	db, err := openDBConnection()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	query := `
+		UPDATE tokens 
+		SET accesstoken = $1, refreshtoken = $2, tokentype = $3, expiry = $4 
+		WHERE id = 1`
+	_, err = db.Exec(query, token.AccessToken, token.RefreshToken, token.TokenType, token.Expiry)
+	if err != nil {
+		return fmt.Errorf("failed to update token: %w", err)
+	}
+
+	fmt.Println("Token updated successfully")
+	return nil
+}
+
+func RetrieveLastToken(token *oauth2.Token) (*oauth2.Token, error) {
+
+	db, err := openDBConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	query := "SELECT accessToken, tokenType, refreshToken, expiry FROM tokens WHERE id = 1"
+	row := db.QueryRow(query)
+
+	err = row.Scan(&token.AccessToken, &token.TokenType, &token.RefreshToken, &token.Expiry)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("no token found")
+		}
+		return nil, fmt.Errorf("failed to retrieve token: %w", err)
+	}
+
+	fmt.Println("Retrieved token successfully")
+
+	return token, nil
+}
